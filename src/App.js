e94ee414de1d63a5d024945d0ac5c31277563441
joao Achao <joao.achao@conecthus.org.br>
2022-10-22 17:14:42 -0400

    testando

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello Word React!</h1>
+    <h2>Emmet funcionou</h2>
+    <h3>Primeiro Projeto da Natalia</h3>
+    <h4>testando cor</h4>
+    </div>
+  );
+}
+
+export default App;
+
+
